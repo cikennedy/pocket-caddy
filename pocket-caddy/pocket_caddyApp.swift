86099f828e2fbfbcc653f43b3cbf630c10f7febb
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-07-06 13:28:55 -0500

    Initial Commit

---
+++ pocket-caddy/pocket_caddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocket_caddyApp.swift
+//  pocket-caddy
+//
+//  Created by Chris Kennedy on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct pocket_caddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
